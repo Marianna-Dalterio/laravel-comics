f2182f5b2b949cbac0014643175878bd3a6a29b1
Marianna-Dalterio <mariannadalterio00@gmail.com>
2026-02-03 23:29:44 +0100

    feat: update footer layout and styles; implement dynamic footer columns

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,57 +1,56 @@
+@php
+    $footer_columns = [
+        [
+            ["title" => "DC COMICS", "links" => ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"]],
+            ["title" => "SHOP", "links" => ["Shop DC", "Shop DC Collectibles"]]
+        ],
+        [
+            ["title" => "DC", "links" => ["Terms Of Use", "Privacy Policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"]]
+        ],
+        [
+            ["title" => "SITES", "links" => ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]]
+        ]
+    ];
+@endphp
+
+
 <footer class="text-white">
-    <div class="footer-main py-5" style="background-color:black; background-size: cover;">
+    <div class="footer-main " style="background-image: url('{{ Vite::asset('resources/img/footer-bg.jpg') }}'); background-size: cover;">
         
-        <div class="container">
+        <div class="container position-relative overflow-hidden py-5">
             <div class="row">
-                <div class="col-2">
-                        <h5 class="fw-bold mb-3">DC COMICS</h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-secondary text-decoration-none">Characters</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Comics</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Movies</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">TV</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Games</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Videos</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">News</a></li>
-                        </ul>
+                
+                @foreach($footer_columns as $column)
+                    <div class="col-2">
+                        @foreach($column as $section)
+                            <h5 class="fw-bold mb-3 mt-3 text-uppercase text-white">{{ $section['title'] }}</h5>
+                            <ul class="list-unstyled">
+                                @foreach($section['links'] as $link)
+                                    <li><a href="#" class="text-secondary text-decoration-none small">{{ $link }}</a></li>
+                                @endforeach
+                            </ul>
+                        @endforeach
+                    </div>
+                @endforeach
 
-                        <h5 class="fw-bold mb-3">SHOP</h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-secondary text-decoration-none">Shop DC</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Shop DC Collaborations</a></li>
-                        </ul>
 
-                        <h5 class="fw-bold mb-3">  DC</h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-secondary text-decoration-none">Terms Of Use</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Privacy Policy</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Ad Choices</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Advertising</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Jobs</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Subscriptions</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Talent Workshops</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">CPSC Certificates</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Ratings</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Shop Help</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">Contact Us</a></li>
-                        </ul>
 
-                        <h5 class="fw-bold mb-3">SITES</h5>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-secondary text-decoration-none"> DC</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">MAD Magazines</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">DC Kids</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">DC Universe</a></li>
-                            <li><a href="#" class="text-secondary text-decoration-none">DC Power Visa</a></li>
-                        </ul>
-
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" 
+                         alt="DC Logo Background" 
+                         class="position-absolute" 
+                         style="right: -50px; top: 50%; transform: translateY(-50%); width: 550px;">
                 </div>
 
+
             </div>
+                
+
+            
         </div>
        
 
-        
+
         <div class="footer-bottom bg-dark py-4">
             <div class="container d-flex justify-content-between align-items-center">
                 <button class="btn btn-outline-primary text-white fw-bold border-2 px-3">SIGN-UP NOW!</button>
